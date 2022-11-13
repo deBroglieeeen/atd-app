16a353f7f36cfe94baf6bbce24861d875fdb4d7e
riko <81846197+riko-okubo@users.noreply.github.com>
2022-11-13 17:02:08 +0900

    feat: 現在時刻を表示

--- atd-app/pages/index.tsx
+++ atd-app/pages/index.tsx
@@ -1,9 +1,23 @@
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
+import dayjs from "dayjs";
 
 const Home: NextPage = () => {
+  const now = dayjs().format("YYYY-MM-DD HH:mm:ss");
+  const [nowTime, setNowime] = useState(now);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      const now = dayjs().format("YYYY-MM-DD HH:mm:ss");
+      setNowime(now);
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <>
       <h1>atd app</h1>
+      <p>{nowTime}</p>
       <button>出勤</button>
       <button>退勤</button>
       <button>休憩</button>

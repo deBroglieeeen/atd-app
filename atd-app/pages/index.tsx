a8c13e4c480e480651c3ec933c69ed4bbf3815b7
riko <81846197+riko-okubo@users.noreply.github.com>
2022-11-30 00:40:08 +0900

    front-hasura & Auth0 connected

--- atd-app/pages/index.tsx
+++ atd-app/pages/index.tsx
@@ -2,6 +2,7 @@ import { NextPage } from "next";
 import { useEffect, useState } from "react";
 import { dayjs } from "../src/lib/dayjs";
 import { Box, Button, Text } from "@chakra-ui/react";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Home: NextPage = () => {
   const now = dayjs().format("YYYY-MM-DD HH:mm:ss");
@@ -11,14 +12,18 @@ const Home: NextPage = () => {
   const [endTime, setEndTime] = useState("");
   const [restStart, setRestStart] = useState("");
   const [restEnd, setRestEnd] = useState("");
+  const { user, isAuthenticated, loginWithRedirect } = useAuth0();
 
   useEffect(() => {
+    if (user === null) {
+      loginWithRedirect();
+    }
     const timer = setInterval(() => {
       const now = dayjs().format("YYYY-MM-DD HH:mm:ss");
       setNowtime(now);
     }, 1000);
     return () => clearInterval(timer);
-  }, []);
+  }, [isAuthenticated, loginWithRedirect]);
 
   return (
     <>
@@ -35,6 +40,7 @@ const Home: NextPage = () => {
       <Button onClick={() => setEndTime(nowTime)}>退勤</Button>
       <Button onClick={() => setRestStart(nowTime)}>休憩</Button>
       <Button onClick={() => setRestEnd(nowTime)}>戻り</Button>
+      <Box>{user ? `(ユーザー:${user?.name}${user.sub})` : null}</Box>
     </>
   );
 };

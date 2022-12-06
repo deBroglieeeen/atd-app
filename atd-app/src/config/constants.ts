23002a0d95c7d0c705f71cf6247e97b3bee2b985
deBroglieeeen <u825246d@ecs.osaka-u.ac.jp>
2022-12-06 15:20:07 +0900

    Delete atd-app directory

--- atd-app/src/config/constants.ts
+++
@@ -1,6 +0,0 @@
-export const AUTH0_DOMAIN = process.env.NEXT_PUBLIC_AUTH0_DOMAIN ?? "";
-export const AUTH0_CLIENT_ID = process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID ?? "";
-export const AUTH0_REDIRECT_URI =
-  process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI ?? "";
-export const AUTH0_API_AUDIENCE = process.env.NEXT_PUBLIC_AUTH0_API_AUDIENCE;
-export const API_HOST = process.env.NEXT_PUBLIC_API_HOST ?? "";
--- atd-app/src/graphql/attendance.ts
+++
@@ -1,7 +0,0 @@
-export const addClockinMutation = /* GraphQL */ `
-  mutation addClockinMutation($startTime: timestamptz!) {
-    insert_attendance_one(object: { start_time: $startTime }) {
-      id
-    }
-  }
-`;

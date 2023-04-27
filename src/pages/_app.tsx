c8b5d7560b9cd4136916de1bc676570fb4dd5411
tomoyuki-hiranuma <numanumatom@gmail.com>
2023-04-28 00:18:17 +0900

    prettier on save setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,15 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import * as React from "react";
-
-// 1. import `ChakraProvider` component
-import { ChakraProvider } from "@chakra-ui/react";
-import { Auth0Provider } from "@auth0/auth0-react";
-import { AuthorizedUrqlProvider } from "../components/AuthorizedUrqlProvider";
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
+import { Auth0Provider } from '@auth0/auth0-react'
+import { AuthorizedUrqlProvider } from '../components/AuthorizedUrqlProvider'
 import {
   AUTH0_API_AUDIENCE,
   AUTH0_CLIENT_ID,
   AUTH0_DOMAIN,
   AUTH0_REDIRECT_URI,
-} from "../config/server";
-import { theme } from "../styles/theme";
+} from '../config/server'
+import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -28,7 +25,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         </AuthorizedUrqlProvider>
       </ChakraProvider>
     </Auth0Provider>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp

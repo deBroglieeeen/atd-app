7adbaff8d70de141b20d40c1c87584ec2791f527
tomoyuki-hiranuma <numanumatom@gmail.com>
2023-04-29 21:33:43 +0900

    データ取得とコンポーネント作成

--- src/components/UpdateAttendanceModal.tsx
+++ src/components/UpdateAttendanceModal.tsx
@@ -2,6 +2,7 @@ import {
   Box,
   Button,
   FormControl,
+  FormLabel,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -9,82 +10,155 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Spinner,
 } from '@chakra-ui/react'
-import { useForm } from 'react-hook-form'
+import { useFieldArray, useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { z } from 'zod'
+import * as z from 'zod'
 import { useAttendanceByDateQuery } from '../hooks/useAttendanceByDate'
-import { useCallback } from 'react'
+import { useCallback, useMemo } from 'react'
 
-type UpdateAttendanceForm = {
-  starts: Date[]
-  ends: Date[]
-  rest_starts: Date[]
-  rest_ends: Date[]
-}
-const UpdateAttendanceSchema = z.object({
-  starts: z.array(z.date()),
-  ends: z.array(z.date()),
-  rest_starts: z.array(z.date()),
-  rest_ends: z.array(z.date()),
+const updateAttendanceSchema = z.object({
+  starts: z.string().array(),
+  ends: z.string().array(),
+  rest_starts: z.string().array(),
+  rest_ends: z.string().array(),
 })
+type UpdateAttendanceSchema = z.infer<typeof updateAttendanceSchema>
 
 type Props = {
   attendanceDate: string
   onClose: () => void
 }
 export const UpdateAttendanceModal = ({ attendanceDate, onClose }: Props) => {
-  const {
-    handleSubmit,
-    register,
-    formState: { errors, isValid, isSubmitting },
-  } = useForm<UpdateAttendanceForm>({
-    resolver: zodResolver(UpdateAttendanceSchema),
-  })
-
-  const onSubmit = useCallback((data: UpdateAttendanceForm) => {}, [])
-
-  // 初期値取得
   const { data, fetching } = useAttendanceByDateQuery(attendanceDate)
-  if (!data || fetching) return <Box>Loading...</Box>
+
+  const defaulatValues: UpdateAttendanceSchema = useMemo(() => {
+    if (!data)
+      return {
+        starts: [],
+        ends: [],
+        rest_starts: [],
+        rest_ends: [],
+      }
+    // Todo: HH:mm形式に変換
+    return {
+      starts: data.attendance?.map((data) => data?.start_time),
+      ends: data.attendance?.map((data) => data?.end_time),
+      rest_starts: data.rest?.map((data) => data?.start_rest),
+      rest_ends: data.rest?.map((data) => data?.end_rest),
+    }
+  }, [data])
 
   return (
     <Modal isOpen onClose={onClose} closeOnOverlayClick={false}>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>勤怠の編集</ModalHeader>
-        <ModalCloseButton type='button' isDisabled={isSubmitting} />
-        <form onSubmit={handleSubmit(onSubmit)}>
+        {fetching ? (
           <ModalBody>
-            {/* 各項目をuseFieldsArrayでレンダリング？？ */}
-            <FormControl>
-              <input {...register('starts')} />
-            </FormControl>
-            <FormControl>
-              <input {...register('ends')} />
-            </FormControl>
-            <FormControl>
-              <input {...register('rest_starts')} />
-            </FormControl>
-            <FormControl>
-              <input {...register('rest_ends')} />
-            </FormControl>
+            <Spinner />
           </ModalBody>
-          <ModalFooter
-            gap='5'
-            justifyContent='center'
-            borderTop='1px solid'
-            borderTopColor='gray.100'
-          >
-            <Button type='button' isDisabled={isSubmitting} onClick={onClose}>
-              キャンセル
-            </Button>
-            <Button colorScheme='blue' type='submit' isDisabled={!isValid}>
-              更新
-            </Button>
-          </ModalFooter>
-        </form>
+        ) : (
+          <UpdateAttendanceModalForm
+            defaultValues={defaulatValues}
+            onClose={onClose}
+          />
+        )}
       </ModalContent>
     </Modal>
+  )
+}
+
+const UpdateAttendanceModalForm = ({
+  defaultValues,
+  onClose,
+}: {
+  defaultValues: UpdateAttendanceSchema
+  onClose: () => void
+}) => {
+  const {
+    handleSubmit,
+    control,
+    register,
+    formState: { isValid, isSubmitting },
+  } = useForm<UpdateAttendanceSchema>({
+    mode: 'onChange',
+    resolver: zodResolver(updateAttendanceSchema),
+    defaultValues: defaultValues,
+  })
+
+  const { fields: startFields } = useFieldArray<UpdateAttendanceSchema>({
+    control,
+    name: 'starts' as never,
+  })
+  const { fields: endFields } = useFieldArray<UpdateAttendanceSchema>({
+    control,
+    name: 'ends' as never,
+  })
+  const { fields: restStartFields } = useFieldArray<UpdateAttendanceSchema>({
+    control,
+    name: 'rest_starts' as never,
+  })
+  const { fields: restEndFields } = useFieldArray<UpdateAttendanceSchema>({
+    control,
+    name: 'rest_ends' as never,
+  })
+
+  const onSubmit = useCallback((data: UpdateAttendanceSchema) => {
+    console.log(data)
+    return ''
+  }, [])
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <ModalBody>
+        {/* 各項目をuseFieldsArrayでレンダリング？？ */}
+        <FormControl>
+          <FormLabel>出勤</FormLabel>
+          {startFields.map((field, index) => (
+            <Box key={field.id}>
+              <input {...register(`starts.${index}`)} />
+            </Box>
+          ))}
+        </FormControl>
+        <FormControl>
+          <FormLabel>退勤</FormLabel>
+          {endFields.map((field, index) => (
+            <Box key={field.id}>
+              <input {...register(`ends.${index}`)} />
+            </Box>
+          ))}
+        </FormControl>
+        <FormControl>
+          <FormLabel>休憩</FormLabel>
+          {restStartFields.map((field, index) => (
+            <Box key={field.id}>
+              <input {...register(`rest_starts.${index}`)} />
+            </Box>
+          ))}
+        </FormControl>
+        <FormControl>
+          <FormLabel>戻り</FormLabel>
+          {restEndFields.map((field, index) => (
+            <Box key={field.id}>
+              <input {...register(`rest_ends.${index}`)} />
+            </Box>
+          ))}
+        </FormControl>
+      </ModalBody>
+      <ModalFooter
+        gap='5'
+        justifyContent='center'
+        borderTop='1px solid'
+        borderTopColor='gray.100'
+      >
+        <Button type='button' isDisabled={isSubmitting} onClick={onClose}>
+          キャンセル
+        </Button>
+        <Button colorScheme='blue' type='submit' isDisabled={!isValid}>
+          更新
+        </Button>
+      </ModalFooter>
+    </form>
   )
 }

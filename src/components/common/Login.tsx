ad10760dbc378a370fdc92fa6a02cef73a721656
tomoyuki-hiranuma <numanumatom@gmail.com>
2023-05-24 21:26:56 +0900

    Login ui adjustment

--- src/components/common/Login.tsx
+++ src/components/common/Login.tsx
@@ -1,15 +1,15 @@
 import { useAuth0 } from '@auth0/auth0-react'
-import { Button, Text } from '@chakra-ui/react'
+import { Button, Flex, Text } from '@chakra-ui/react'
 
 const Login = () => {
   const { loginWithRedirect } = useAuth0()
 
   return (
-    <>
+    <Flex justifyContent='center' flexDir='column' alignItems='center'>
       <Text>お疲れ様です！</Text>
       <Text>ログインしてから打刻してください _(._.)_</Text>
       <Button onClick={loginWithRedirect}>Log in</Button>
-    </>
+    </Flex>
   )
 }
 
